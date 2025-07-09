64541f0d1bc479aacd294efca476d584c48dd9a4
Ringsat <ringsatdaimary87@gmail.com>
2025-07-09 18:17:04 +0530

    Update play.js

--- Commands/play.js
+++ Commands/play.js
@@ -1,20 +1,18 @@
 module.exports = {
   name: 'play',
   async execute(client, message, args) {
+    const { DisTube } = require('distube');
+    const distube = new DisTube(client, { emitNewSongOnly: true });
+
     const voiceChannel = message.member.voice.channel;
-    if (!voiceChannel) return message.reply('❌ Join a voice channel first!');
+    if (!voiceChannel) return message.reply("❌ Join a voice channel first!");
 
-    const query = args.join(' ');
-    if (!query) return message.reply('❌ Please provide a song name or link.');
+    const song = args.join(" ");
+    if (!song) return message.reply("❌ Type a song name or YouTube link!");
 
-    try {
-      client.distube.play(voiceChannel, query, {
-        textChannel: message.channel,
-        member: message.member,
-      });
-    } catch (error) {
-      console.error(error);
-      message.reply('⚠️ Error playing music.');
-    }
+    distube.play(voiceChannel, song, {
+      textChannel: message.channel,
+      member: message.member
+    });
   }
 };
